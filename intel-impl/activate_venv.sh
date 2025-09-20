45556a794ca82c96d42c95727f86aa64dec3de3e
Sree Kommalapati <sree.n.kommalapati@gmail.com>
2025-09-19 23:05:47 -0400

    move intel lib into subfolder

---
+++ intel-impl/activate_venv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Convenience script to activate the virtual environment
+cd /home/sree/Projects/ndg-fitting
+source venv/bin/activate
+echo "Virtual environment activated for NDG Fitting project!"
+echo "Python version: $(python --version)"
+echo "To deactivate, run: deactivate"
